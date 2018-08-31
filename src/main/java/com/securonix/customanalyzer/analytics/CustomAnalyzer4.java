8ecb43af1c59363c5434851398f9ed1b5d7e85fe
Manish Kumar <manish1746@gmail.com>
2018-08-31 20:51:29 +0530

    Added support to insert/fetch sample data into/from Hbase table.

--- src/main/java/com/securonix/customanalyzer/analytics/CustomAnalyzer4.java
+++ src/main/java/com/securonix/customanalyzer/analytics/CustomAnalyzer4.java
@@ -5,24 +5,25 @@
  */
 package com.securonix.customanalyzer.analytics;
 
+import com.securonix.customanalyzer.hbaseutil.dao.EmailRecipientCountDAO;
 import com.securonix.hbasecorrelator.dao.CorrelationRulesHDAO;
 import com.securonix.hbaseutil.HBaseClient;
 import com.securonix.hbaseutil.HBaseConstants;
 import static com.securonix.hbaseutil.HBaseConstants.TABLE_CORRELATION_RULES_H;
 import static com.securonix.hbaseutil.HBaseConstants.TABLE_VIOLATIONS;
-import com.securonix.hbaseutil.HBaseException;
 import com.securonix.hbaseutil.dao.AbstractTableDAO;
 import com.securonix.hbaseutil.dao.KafkaOffsetDAO;
 import com.securonix.hbaseutil.dao.SnyperErrorDAO;
 import com.securonix.hbaseutil.dao.ViolationDAO;
 import com.securonix.snyper.common.MiniEEO;
-import com.securonix.snyper.common.SnyperException;
 import com.securonix.snyper.config.beans.HBaseConfigBean;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import com.securonix.customanalyzer.hbaseutil.beans.EmailRecipientCount;
+import org.apache.hadoop.hbase.filter.Filter;
 
 /**
  *
@@ -33,13 +34,14 @@ public class CustomAnalyzer4 {
     private final Logger LOGGER = LogManager.getLogger();
 
     HBaseClient hbaseClient = null;
-    
+
+    EmailRecipientCountDAO eCountDAO;
+
     /**
      * Hbase Client gets initialized.
-     * 
+     *
      * @param hbaseConfigBean Hbase Configuration Bean
      */
-
     public void init(final HBaseConfigBean hbaseConfigBean) {
 
         hbaseClient = new HBaseClient();
@@ -47,20 +49,68 @@ public class CustomAnalyzer4 {
             hbaseClient.initializeHBase(hbaseConfigBean);
             LOGGER.debug("HBase client initialized!");
 
+            eCountDAO = new EmailRecipientCountDAO(hbaseClient);
+
+            try {
+
+                // Createt Hbase Table (tenantname:emailrecipientcount)
+                eCountDAO.createTable();
+
+                //Publish sample data into Hbase table
+                publishSampleRecords();
+
+                // Fetch Hbase tabe records
+                List<EmailRecipientCount> records = getHbaseRecords();
+
+            } catch (Exception ex) {
+                LOGGER.error("Hbase operations failed",ex);
+            }
+
         } catch (Exception ex) {
             LOGGER.error("Failed to initialized Hbase client", ex);
 
         }
 
     }
-    
+
+    public void insertRecord(EmailRecipientCount emailRecipientCount) {
+
+        try {
+            eCountDAO.insert(emailRecipientCount);
+        } catch (IOException ex) {
+            LOGGER.error("Failed to insert record into hbase", ex);
+
+        }
+
+    }
+
+    public void publishSampleRecords() {
+        EmailRecipientCount emailRecipientCount = new EmailRecipientCount();
+        emailRecipientCount.setPk("mkumar@securonix.com");
+        emailRecipientCount.setEmailRecipient("mkumar@securonix.com");
+        emailRecipientCount.setCount(3);
+        insertRecord(emailRecipientCount);
+
+    }
+
+    public List<EmailRecipientCount> getHbaseRecords() {
+        final Filter filter = null;
+        List<EmailRecipientCount> list = new ArrayList<>();
+        try {
+            list = eCountDAO.read(filter);
+        } catch (IOException ex) {
+            LOGGER.error("Failed to get record from hbase", ex);
+        }
+
+        return list;
+    }
+
     /**
      * Fetch records from Hbase table
-     * 
+     *
      * @param table : Hbase table name
      * @return List of Hbase records
      */
-
     public List getHbaseRecords(String table) {
         List<Object> records = null;
         try {
